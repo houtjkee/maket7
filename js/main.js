2213b27793cf4cbc26457139245ec671ef509fa0
houtjkee <kyzmyksashok@gmail.com>
2023-05-12 17:59:35 +0300

    create media

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.banner__slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000
+  });
+});
