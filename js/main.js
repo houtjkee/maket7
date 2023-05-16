93890ade41c2fca30c1070af4efd04480ae829b1
houtjkee <kyzmyksashok@gmail.com>
2023-05-16 17:54:04 +0300

    create block share

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Banner slider */
 $(document).ready(function () {
   $('.banner__slider').slick({
     dots: true,
@@ -10,9 +11,23 @@ $(document).ready(function () {
   });
 });
 
+/* More than 3 elements */
 if (document.querySelectorAll('.ama-item').length > 3) {
   const elements = document.querySelectorAll('.ama-item');
   elements.forEach((element) => {
     element.style.margin = '0';
   });
 }
+
+/* Share slider */
+$(document).ready(function () {
+  $('.share__info-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000
+  });
+});
