7cb00a6e652bcdf5695698e777c9d95b7a006049
houtjkee <kyzmyksashok@gmail.com>
2023-05-19 17:54:08 +0300

    Create block gallery & add some function

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ $(document).ready(function () {
   });
 });
 
+/* Features slider */
 $(document).ready(function () {
   $('.features__info-slider').slick({
     dots: true,
@@ -42,3 +43,44 @@ $(document).ready(function () {
     autoplaySpeed: 7000,
   });
 });
+
+/* Gallery overlay */
+const galleryImages = document.querySelectorAll('.gallery__images__block .gallery__images-item');
+
+galleryImages.forEach((galleryImage) => {
+  const overlay = galleryImage.querySelector('.overlay');
+
+  overlay.addEventListener('mouseover', function() {
+    galleryImage.classList.add('hovered');
+  });
+
+  overlay.addEventListener('mouseout', function() {
+    galleryImage.classList.remove('hovered');
+  });
+});
+
+
+
+/* Like counter */
+const likeButtons = document.querySelectorAll('.like-button');
+
+likeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const paintedHeart = button.querySelector('.like-button-img-painted');
+    const regularHeart = button.querySelector('.like-button-img');
+    const likeCount = button.closest('.like-section').querySelector('.like-count');
+
+    const isLiked = paintedHeart.style.display === 'block';
+
+    if (isLiked) {
+      paintedHeart.style.display = 'none';
+      regularHeart.style.display = 'block';
+      likeCount.textContent = parseInt(likeCount.textContent) - 1;
+    } else {
+      paintedHeart.style.display = 'block';
+      regularHeart.style.display = 'none';
+      likeCount.textContent = parseInt(likeCount.textContent) + 1;
+    }
+  });
+});
+
